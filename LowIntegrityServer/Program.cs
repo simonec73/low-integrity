4c57b49ecbf0d8d4fbbf7ea2785e33f412e239fc
Simone Curzi <simone.curzi@microsoft.com>
2025-05-14 12:44:08 +0200

    Initial implementation

---
+++ LowIntegrityServer/Program.cs
@@ -0,0 +1,218 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO.Pipes;
+using System.Runtime.InteropServices;
+using System.Security.AccessControl;
+using System.Security.Principal;
+
+namespace LowIntegrityServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Integrity Level: " + GetProcessIntegrityLevelDescription());
+            NamedPipeServerStream pipeServer = new NamedPipeServerStream(@"LowIntegrityNamedPipe",
+                PipeDirection.InOut, 100, PipeTransmissionMode.Byte, PipeOptions.Asynchronous, 0, 0);
+            pipeServer.BeginWaitForConnection(HandleConnection, pipeServer);
+
+            Console.WriteLine("Waiting...");
+            Console.ReadLine();
+        }
+
+        private static void HandleConnection(IAsyncResult ar)
+        {
+            Console.WriteLine("Connection established.");
+
+            NamedPipeServerStream pipeServer = ar.AsyncState as NamedPipeServerStream;
+
+            if (pipeServer != null)
+            {
+                pipeServer.EndWaitForConnection(ar);
+
+                if (pipeServer.IsConnected)
+                {
+                    var bytes = System.Text.Encoding.UTF8.GetBytes("This is the output from the Server!");
+                    pipeServer.Write(bytes, 0, bytes.Length);
+                    pipeServer.Flush();
+
+                    Console.WriteLine("Message sent.");
+
+                    byte[] buffer = new byte[100];
+                    bool close = false;
+
+                    do
+                    {
+                        int len = pipeServer.Read(buffer, 0, 100);
+
+                        string text = System.Text.Encoding.UTF8.GetString(buffer, 0, len);
+
+                        if (text == "Close")
+                        {
+                            close = true;
+                        } else if (text == "GetAnotherString")
+                        {
+                            bytes = System.Text.Encoding.UTF8.GetBytes("This is another string!");
+                            pipeServer.Write(bytes, 0, bytes.Length);
+                            pipeServer.Flush();
+
+                            Console.WriteLine("Another message sent.");
+                        }
+                    } while (!close);
+
+                    pipeServer.Close();
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("Client is not connected!");
+                }
+            }
+        }
+
+        #region Helper Functions related to Process Integrity Level
+        /// <summary>
+        /// The function gets the integrity level of the current process. Integrity 
+        /// level is only available on Windows Vista and newer operating systems, thus 
+        /// GetProcessIntegrityLevel throws a C++ exception if it is called on systems 
+        /// prior to Windows Vista.
+        /// </summary>
+        /// <returns>
+        /// Returns the integrity level of the current process. It is usually one of 
+        /// these values:
+        /// 
+        ///    SECURITY_MANDATORY_UNTRUSTED_RID - means untrusted level. It is used 
+        ///    by processes started by the Anonymous group. Blocks most write access.
+        ///    (SID: S-1-16-0x0)
+        ///    
+        ///    SECURITY_MANDATORY_LOW_RID - means low integrity level. It is used by
+        ///    Protected Mode Internet Explorer. Blocks write acess to most objects 
+        ///    (such as files and registry keys) on the system. (SID: S-1-16-0x1000)
+        /// 
+        ///    SECURITY_MANDATORY_MEDIUM_RID - means medium integrity level. It is 
+        ///    used by normal applications being launched while UAC is enabled. 
+        ///    (SID: S-1-16-0x2000)
+        ///    
+        ///    SECURITY_MANDATORY_HIGH_RID - means high integrity level. It is used 
+        ///    by administrative applications launched through elevation when UAC is 
+        ///    enabled, or normal applications if UAC is disabled and the user is an 
+        ///    administrator. (SID: S-1-16-0x3000)
+        ///    
+        ///    SECURITY_MANDATORY_SYSTEM_RID - means system integrity level. It is 
+        ///    used by services and other system-level applications (such as Wininit, 
+        ///    Winlogon, Smss, etc.)  (SID: S-1-16-0x4000)
+        /// 
+        /// </returns>
+        /// <exception cref="System.ComponentModel.Win32Exception">
+        /// When any native Windows API call fails, the function throws a Win32Exception 
+        /// with the last error code.
+        /// </exception>
+        internal static int GetProcessIntegrityLevel()
+        {
+            int IL = -1;
+            SafeTokenHandle hToken = null;
+            int cbTokenIL = 0;
+            IntPtr pTokenIL = IntPtr.Zero;
+
+            try
+            {
+                // Open the access token of the current process with TOKEN_QUERY.
+                if (!NativeMethod.OpenProcessToken(Process.GetCurrentProcess().Handle,
+                    NativeMethod.TOKEN_QUERY, out hToken))
+                {
+                    throw new Win32Exception();
+                }
+
+                // Then we must query the size of the integrity level information 
+                // associated with the token. Note that we expect GetTokenInformation 
+                // to return false with the ERROR_INSUFFICIENT_BUFFER error code 
+                // because we've given it a null buffer. On exit cbTokenIL will tell 
+                // the size of the group information.
+                if (!NativeMethod.GetTokenInformation(hToken,
+                    TOKEN_INFORMATION_CLASS.TokenIntegrityLevel, IntPtr.Zero, 0,
+                    out cbTokenIL))
+                {
+                    int error = Marshal.GetLastWin32Error();
+                    if (error != NativeMethod.ERROR_INSUFFICIENT_BUFFER)
+                    {
+                        // When the process is run on operating systems prior to 
+                        // Windows Vista, GetTokenInformation returns false with the 
+                        // ERROR_INVALID_PARAMETER error code because 
+                        // TokenIntegrityLevel is not supported on those OS's.
+                        throw new Win32Exception(error);
+                    }
+                }
+
+                // Now we allocate a buffer for the integrity level information.
+                pTokenIL = Marshal.AllocHGlobal(cbTokenIL);
+                if (pTokenIL == IntPtr.Zero)
+                {
+                    throw new Win32Exception();
+                }
+
+                // Now we ask for the integrity level information again. This may fail 
+                // if an administrator has added this account to an additional group 
+                // between our first call to GetTokenInformation and this one.
+                if (!NativeMethod.GetTokenInformation(hToken,
+                    TOKEN_INFORMATION_CLASS.TokenIntegrityLevel, pTokenIL, cbTokenIL,
+                    out cbTokenIL))
+                {
+                    throw new Win32Exception();
+                }
+
+                // Marshal the TOKEN_MANDATORY_LABEL struct from native to .NET object.
+                TOKEN_MANDATORY_LABEL tokenIL = (TOKEN_MANDATORY_LABEL)
+                    Marshal.PtrToStructure(pTokenIL, typeof(TOKEN_MANDATORY_LABEL));
+
+                // Integrity Level SIDs are in the form of S-1-16-0xXXXX. (e.g. 
+                // S-1-16-0x1000 stands for low integrity level SID). There is one 
+                // and only one subauthority.
+                IntPtr pIL = NativeMethod.GetSidSubAuthority(tokenIL.Label.Sid, 0);
+                IL = Marshal.ReadInt32(pIL);
+            }
+            finally
+            {
+                // Centralized cleanup for all allocated resources. 
+                if (hToken != null)
+                {
+                    hToken.Close();
+                    hToken = null;
+                }
+                if (pTokenIL != IntPtr.Zero)
+                {
+                    Marshal.FreeHGlobal(pTokenIL);
+                    pTokenIL = IntPtr.Zero;
+                    cbTokenIL = 0;
+                }
+            }
+
+            return IL;
+        }
+        
+        internal static string GetProcessIntegrityLevelDescription()
+        {
+            string result;
+
+            switch (GetProcessIntegrityLevel())
+            {
+                case NativeMethod.SECURITY_MANDATORY_LOW_RID:
+                    result = "LOW";
+                    break;
+                case NativeMethod.SECURITY_MANDATORY_MEDIUM_RID:
+                    result = "MEDIUM";
+                    break;
+                case NativeMethod.SECURITY_MANDATORY_HIGH_RID:
+                    result = "HIGH";
+                    break;
+                default:
+                    result = "Unknown";
+                    break;
+            }
+
+            return result;
+        }
+        #endregion
+    }
+}
---
+++ MediumIntegrityClient/MainForm.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace MediumIntegrityClient
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this._connect = new System.Windows.Forms.Button();
+            this._text = new System.Windows.Forms.TextBox();
+            this._close = new System.Windows.Forms.Button();
+            this._getAnotherString = new System.Windows.Forms.Button();
+            this._startServer = new System.Windows.Forms.Button();
+            this._hiddenServer = new System.Windows.Forms.CheckBox();
+            this._clear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // _connect
+            // 
+            this._connect.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._connect.Enabled = false;
+            this._connect.Location = new System.Drawing.Point(24, 98);
+            this._connect.Name = "_connect";
+            this._connect.Size = new System.Drawing.Size(655, 50);
+            this._connect.TabIndex = 2;
+            this._connect.Text = "Connect";
+            this._connect.UseVisualStyleBackColor = true;
+            this._connect.Click += new System.EventHandler(this._connect_Click);
+            // 
+            // _text
+            // 
+            this._text.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._text.Location = new System.Drawing.Point(24, 154);
+            this._text.Multiline = true;
+            this._text.Name = "_text";
+            this._text.ReadOnly = true;
+            this._text.Size = new System.Drawing.Size(655, 359);
+            this._text.TabIndex = 3;
+            // 
+            // _close
+            // 
+            this._close.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._close.Enabled = false;
+            this._close.Location = new System.Drawing.Point(24, 631);
+            this._close.Name = "_close";
+            this._close.Size = new System.Drawing.Size(655, 50);
+            this._close.TabIndex = 6;
+            this._close.Text = "Close";
+            this._close.UseVisualStyleBackColor = true;
+            this._close.Click += new System.EventHandler(this._close_Click);
+            // 
+            // _getAnotherString
+            // 
+            this._getAnotherString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._getAnotherString.Enabled = false;
+            this._getAnotherString.Location = new System.Drawing.Point(24, 575);
+            this._getAnotherString.Name = "_getAnotherString";
+            this._getAnotherString.Size = new System.Drawing.Size(655, 50);
+            this._getAnotherString.TabIndex = 5;
+            this._getAnotherString.Text = "Get Another String";
+            this._getAnotherString.UseVisualStyleBackColor = true;
+            this._getAnotherString.Click += new System.EventHandler(this._getAnotherString_Click);
+            // 
+            // _startServer
+            // 
+            this._startServer.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._startServer.Location = new System.Drawing.Point(24, 12);
+            this._startServer.Name = "_startServer";
+            this._startServer.Size = new System.Drawing.Size(655, 50);
+            this._startServer.TabIndex = 0;
+            this._startServer.Text = "Start Server";
+            this._startServer.UseVisualStyleBackColor = true;
+            this._startServer.Click += new System.EventHandler(this._startServer_Click);
+            // 
+            // _hiddenServer
+            // 
+            this._hiddenServer.AutoSize = true;
+            this._hiddenServer.Location = new System.Drawing.Point(24, 68);
+            this._hiddenServer.Name = "_hiddenServer";
+            this._hiddenServer.Size = new System.Drawing.Size(185, 24);
+            this._hiddenServer.TabIndex = 1;
+            this._hiddenServer.Text = "Create hidden Server";
+            this._hiddenServer.UseVisualStyleBackColor = true;
+            // 
+            // _clear
+            // 
+            this._clear.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this._clear.Location = new System.Drawing.Point(24, 519);
+            this._clear.Name = "_clear";
+            this._clear.Size = new System.Drawing.Size(655, 50);
+            this._clear.TabIndex = 4;
+            this._clear.Text = "Clear Text";
+            this._clear.UseVisualStyleBackColor = true;
+            this._clear.Click += new System.EventHandler(this._clear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 693);
+            this.Controls.Add(this._hiddenServer);
+            this.Controls.Add(this._text);
+            this.Controls.Add(this._startServer);
+            this.Controls.Add(this._clear);
+            this.Controls.Add(this._getAnotherString);
+            this.Controls.Add(this._close);
+            this.Controls.Add(this._connect);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button _connect;
+        private System.Windows.Forms.TextBox _text;
+        private System.Windows.Forms.Button _close;
+        private System.Windows.Forms.Button _getAnotherString;
+        private System.Windows.Forms.Button _startServer;
+        private System.Windows.Forms.CheckBox _hiddenServer;
+        private System.Windows.Forms.Button _clear;
+    }
+}
+
